990da1d8ddd08ee30244f9862d56bfde855e5eb5
davi-garcia <davi100msn@gmail.com>
2024-03-27 19:22:31 -0300

    teste3

--- converter.js
+++ converter.js
@@ -1,22 +1,3 @@
-self.onmessage = function (event) {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d");
-      const img = new Image();
-      img.onload = () => {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-        const result = canvas.toDataURL("image/webp");
-        const binary = atob(result.split(",")[1]);
-        const array = new Uint8Array(binary.length);
-        for (let i = 0; i < binary.length; i++) {
-          array[i] = binary.charCodeAt(i);
-        }
-        self.postMessage({ index: event.data.index, binary: array }, [array.buffer]);
-      };
-      img.src = event.target.result;
-    };
-    reader.readAsDataURL(event.data.file);
-  };
+self.addEventListener('message', (event) => {
+    const file = event.data;
+   
--- script.js
+++ script.js
@@ -1,19 +1,67 @@
-function convertImages() {
-  const input = document.getElementById("image-input");
-  const output = document.getElementById("output");
-  const files = input.files;
-  const converter = new Worker("converter.js");
-  converter.onmessage = (event) => {
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(event.data);
-    link.download = files[event.data.index].name.replace(/\.jpg$/, ".webp");
-    link.click();
-    output.innerHTML += `Converted image ${event.data.index} to WEBP.<br>`;
-  };
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    if (file.type.startsWith("image/jpeg")) {
-      converter.postMessage({ index: i, file: file });
+const fileInput = document.getElementById('file-input');
+const convertButton = document.getElementById('convert-button');
+const output = document.getElementById('output');
+const downloadButton = document.getElementById('download-button');
+
+const conversionQueue = [];
+let currentConversionIndex = 0;
+
+fileInput.addEventListener('change', (event) => {
+    const files = event.target.files;
+    conversionQueue.length = 0;
+    currentConversionIndex = 0;
+    output.innerHTML = '';
+    downloadButton.disabled = true;
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        if (file.type.startsWith('image/jpeg') || file.type.startsWith('image/jpg')) {
+            conversionQueue.push(file);
+        }
     }
-  }
+    convertButton.disabled = conversionQueue.length === 0;
+});
+
+convertButton.addEventListener('click', () => {
+    convertButton.disabled = true;
+    const worker = new Worker('converter.js');
+    worker.postMessage(conversionQueue[currentConversionIndex]);
+    worker.addEventListener('message', (event) => {
+        if (event.data.type === 'progress') {
+            output.innerHTML += `<p>Converting ${conversionQueue[currentConversionIndex].name} ${Math.round(event.data.percentage * 100)}%</p>`;
+        } else if (event.data.type === 'result') {
+            output.innerHTML += `<p>Converted ${conversionQueue[currentConversionIndex].name}</p>`;
+            currentConversionIndex++;
+            if (currentConversionIndex < conversionQueue.length) {
+                worker.postMessage(conversionQueue[currentConversionIndex]);
+            } else {
+                convertButton.disabled = false;
+                downloadButton.disabled = false;
+                worker.terminate();
+            }
+        }
+    });
+    if (currentConversionIndex === 0) {
+        worker.postMessage(conversionQueue[currentConversionIndex]);
+    }
+});
+
+function downloadAll() {
+    const zip = new JSZip();
+    const promises = [];
+    for (let i = 0; i < conversionQueue.length; i++) {
+        const file = conversionQueue[i];
+        const filename = file.name.replace(/\.[^/.]*$/, '').toLowerCase();
+        promises.push(
+            fetch(URL.createObjectURL(file)).then((response) => response.blob())
+            .then((blob) => zip.file(`${filename}.webp`, blob))
+            .then(() => URL.revokeObjectURL(blob))
+        );
+    }
+    Promise.all(promises).then(() => {
+        zip.generateAsync({ type: 'blob' }).then((blob) => {
+            saveAs(blob, 'ConvertedImages.zip');
+        });
+    });
 }
+
+downloadButton.addEventListener('click', downloadAll);
